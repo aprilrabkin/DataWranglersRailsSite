bbf5fff537d77ad3e0fe693a93b4e63fb8f3ea7d
April Rabkin <april.rabkin@flatironschool.com>
2015-01-10 23:59:02 -0500

    rudimentary team page and link

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
 
   resources :home, only: [:index]
 
+  resources :team, only: [:index]
+  
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
