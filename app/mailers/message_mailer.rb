51d37c5323e01fcca8c243eed2497659d2909269
April Rabkin <april.rabkin@flatironschool.com>
2015-03-06 12:12:26 -0500

    changed default from in the mailer to my own email address

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,7 @@
 class MessageMailer < ActionMailer::Base
 
-  default from: "Your Mailer <noreply@yourdomain.com>"
+#  default from: "Your Mailer <noreply@yourdomain.com>"
+  default from: "<april.rabkin@gmail.com>"
   default to: "Your Name <april.rabkin@gmail.com>"
 
   def new_message(message)
